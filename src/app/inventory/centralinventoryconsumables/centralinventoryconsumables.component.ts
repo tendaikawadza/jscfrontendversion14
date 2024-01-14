7939886f12939a007af6ef8be3e376a689f335e8
tendaikawadza <tendaikawadza49@gmail.com>
2024-01-14 15:52:50 +0200

    first commit

---
+++ src/app/inventory/centralinventoryconsumables/centralinventoryconsumables.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-centralinventoryconsumables',
+  templateUrl: './centralinventoryconsumables.component.html',
+  styleUrls: ['./centralinventoryconsumables.component.css']
+})
+export class CentralinventoryconsumablesComponent {
+hideDialog() {
+throw new Error('Method not implemented.');
+}
+submitted: any;
+f: any;
+onSubmit() {
+throw new Error('Method not implemented.');
+}
+productDialog: any;
+addProductForm: FormGroup<any>;
+editProduct(_t124: any) {
+throw new Error('Method not implemented.');
+}
+products: any[];
+logout() {
+throw new Error('Method not implemented.');
+}
+getPcode(arg0: any) {
+throw new Error('Method not implemented.');
+}
+pcodeList: any[];
+selectedCode: any;
+openNew() {
+throw new Error('Method not implemented.');
+}
+selectedProducts: any;
+
+}
